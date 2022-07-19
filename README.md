211af5b9d5e27f674959f7ba96d651304459cab8
Yeongju Kang <yeongjukang@outlook.com>
2022-07-19 22:14:06 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# docker-odin
+This is ths Git repo of the UNOFFICIAL IMAGE for Odin programming language.
---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+ALPINE_VERSION="3.16"
+DEBIAN_VERSION="bullseye"
+ODIN_VERSION="dev-2022-07"
+
+mkdir -p $ODIN_VERSION/alpine
+mkdir -p $ODIN_VERSION/debian
+
+sed "s/\^ALPINE_VERSION/$ALPINE_VERSION/g" Dockerfile-alpine.template > $ODIN_VERSION/alpine/Dockerfile
+sed "s/\^DEBIAN_VERSION/$DEBIAN_VERSION/g" Dockerfile-debian.template > $ODIN_VERSION/debian/Dockerfile
+sed -i "s/\^ODIN_VERSION/$ODIN_VERSION/g" $ODIN_VERSION/alpine/Dockerfile
+sed -i "s/\^ODIN_VERSION/$ODIN_VERSION/g" $ODIN_VERSION/debian/Dockerfile
